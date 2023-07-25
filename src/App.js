d49bb83db9875b66adbea5ca4d356f5ec6d8e602
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2023-07-25 22:28:26 +0530

    added dark/light theme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import About from './Components/About';
 function App() {
   return (
    <>
    <Navbar title="MyApp" aboutText="About Us"/>
    <div className="container">
    <TextForm heading='Enter text you want to analyze'/>
+   <About/>
    </div>
    </>
   );
