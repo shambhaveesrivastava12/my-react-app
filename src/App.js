ea84994423a8d4690b21c61b63c7e5b95aec8895
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2023-07-25 10:57:57 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 function App() {
   return (
    <>
    <Navbar title="MyApp" aboutText="About Us"/>
+   <div className="container">
+   <TextForm heading='Enter text you want to analyze'/>
+   </div>
    </>
   );
 }
