7d4a7b57375b5a6212954fbf2131a514d85851b3
Shambhavee Srivastava <110680194+shambhaveesrivastava12@users.noreply.github.com>
2023-07-26 11:40:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-import About from './Components/About';
+
+// import About from './Components/About';
 function App() {
+  const [mode, setMode]= useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#161616'
+    }
+    if(mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
    <>
-   <Navbar title="MyApp" aboutText="About Us"/>
+   <Navbar title="MyApp" aboutText="About Us" mode = {mode} toggleMode={toggleMode}/>
    <div className="container">
-   <TextForm heading='Enter text you want to analyze'/>
-   <About/>
+   <TextForm heading='Enter text you want to analyze' mode={mode}/>
+   {/* <About/> */}
    </div>
    </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,20 +1,19 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-  const convertUptext = () =>{
-    // console.log("UpperCase was clicked" + text);
-    let newtext = text.toUpperCase();
-    setText(newtext);
+  const convertUptext = ()=>{
+      let newText = text.toUpperCase();
+      setText(newText)
   }
   const convertLotext = () =>{
     // console.log("LowerCase was clicked" + text);
     let newtext = text.toLowerCase();
-    setText(newtext);
+    setText(newtext)
   }
   const convertClear = () =>{
     // console.log("LowerCase was clicked" + text);
     let newtext = '';
-    setText(newtext);
+    setText(newtext)
   }
   const convertReverse = () =>{
     // console.log("LowerCase was clicked" + text);
@@ -30,25 +29,22 @@ export default function TextForm(props) {
   const [text, setText] = useState('');
   return (
     <>
-    <div className="container">
-      <form>
-      <h1>{props.heading}</h1>
-          <div className="mb-3">
-            <input type="text" className="form-control" value = {text} onChange={handleOnChange} id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          </div>
-          <button type="submit" className="btn btn-primary mx-2" onClick ={convertUptext}>Convert to UpperCase</button>
-          <button type="submit" className="btn btn-primary mx-2" onClick ={convertLotext}>Convert to LowerCase</button>
-          <button type="submit" className="btn btn-primary mx-2" onClick ={convertClear}>Clear text</button>
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}> 
+        <h1 className='mb-4'>{props.heading}</h1>
+        <div className="mb-3"> 
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#161616':'white', color: props.mode==='dark'?'white':'#161616'}} id="myBox" rows="8"></textarea>
+        </div>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={convertUptext}>Convert to Uppercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={convertLotext}>Convert to Lowercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={convertClear}>Clear Text</button>
           <button type="submit" className="btn btn-primary mx-2" onClick ={convertReverse}>Reverse text</button>
-
-        </form>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
       <h2>Your text summary</h2>
       <p>{text.split(" ").length - 1} words {text.length} characters </p>
       <p>{ 0.008 * text.split(" ").length} minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:'Enter something to preview it!'}</p>
     </div>
     </>
   )
